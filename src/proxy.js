01d0b71b6a2a12a0ad6b852cdebcad6a3ea33515
souravnath4587-cmd <souravnath4587@gmail.com>
2026-05-02 12:21:46 +0600

    working on proxy.js

---
+++ src/proxy.js
@@ -0,0 +1,21 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/login", request.url));
+}
+
+export const config = {
+  matcher: ["/animals/:path*", "/contact"],
+};
